92bd6d4bd8abaecb92452ed89872d6bbb51cb3ec
S.Y.L <784838898@qq.com>
2017-03-10 18:04:00 +0800

    add Invitation Service

--- hot-deploy/pewebview/src/com/banfftech/personerp/webviewservice/InvitationService.java
+++ hot-deploy/pewebview/src/com/banfftech/personerp/webviewservice/InvitationService.java
@@ -1,7 +1,24 @@
 package com.banfftech.personerp.webviewservice;
 
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
 /**
- * Created by Administrator on 2017/3/10.
+ * Created by S.Y.L on 2017/3/10.
  */
 public class InvitationService {
+
+}
+    public static Map<String, Object> findMyEventDetail(DispatchContext dctx, Map<String, Object> context)
+            throws GenericEntityException {
+        LocalDispatcher dispatcher = dctx.getDispatcher();
+        Delegator delegator = dispatcher.getDelegator();
+        GenericValue userLogin = (GenericValue) context.get("userLogin");
+        String partyId = (String) userLogin.get("partyId");
+        Locale locale = (Locale) context.get("locale");
+        Map<String, Object> inputMap = new HashMap<String, Object>();
+        Map<String, Object> resultMap = ServiceUtil.returnSuccess();
+        String workEffortId = (String) context.get("workEffortId");
+
 }
